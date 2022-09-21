fc473503ba65b38955e02ff8536f7f4cca76acff
jeremy-tesh <jermitesh20@gmail.com>
2022-09-21 19:08:46 +0300

    fix:arrow usage error

--- apps/meteor/client/sidebar/header/actions/Search.tsx
+++ apps/meteor/client/sidebar/header/actions/Search.tsx
@@ -3,45 +3,49 @@ import React, { VFC, HTMLAttributes } from 'react';
 
 import SearchList from '../../search/SearchList';
 
-const Search: VFC<Omit<HTMLAttributes<HTMLElement>, 'is'>> = () => {
-	// const [searchOpen, setSearchOpen] = useState(false);
+const Search: VFC<Omit<HTMLAttributes<HTMLElement>, 'is'>> = () => (
+	<Box w='full'>
+		<SearchList />
+	</Box>
+);
+// 	// const [searchOpen, setSearchOpen] = useState(false);
 
-	// const ref = useRef<HTMLElement>(null);
-	// const handleCloseSearch = useMutableCallback(() => {
-	// 	setSearchOpen(false);
-	// });
+// 	// const ref = useRef<HTMLElement>(null);
+// 	// const handleCloseSearch = useMutableCallback(() => {
+// 	// 	setSearchOpen(false);
+// 	// });
 
-	// useOutsideClick([ref], handleCloseSearch);
+// 	// useOutsideClick([ref], handleCloseSearch);
 
-	// const openSearch = useMutableCallback(() => {
-	// 	setSearchOpen(true);
-	// });
+// 	// const openSearch = useMutableCallback(() => {
+// 	// 	setSearchOpen(true);
+// 	// });
 
-	// useEffect(() => {
-	// 	const unsubscribe = tinykeys(window, {
-	// 		'$mod+K': (event) => {
-	// 			event.preventDefault();
-	// 			openSearch();
-	// 		},
-	// 		'$mod+P': (event) => {
-	// 			event.preventDefault();
-	// 			openSearch();
-	// 		},
-	// 	});
+// 	// useEffect(() => {
+// 	// 	const unsubscribe = tinykeys(window, {
+// 	// 		'$mod+K': (event) => {
+// 	// 			event.preventDefault();
+// 	// 			openSearch();
+// 	// 		},
+// 	// 		'$mod+P': (event) => {
+// 	// 			event.preventDefault();
+// 	// 			openSearch();
+// 	// 		},
+// 	// 	});
 
-	// 	return (): void => {
-	// 		unsubscribe();
+// 	// 	return (): void => {
+// 	// 		unsubscribe();
 
-	// 	};
-	// }, [openSearch]);
+// 	// 	};
+// 	// }, [openSearch]);
 
-	return (
-		<>
-			<Box w='full'>
-				<SearchList />
-			</Box>
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<Box w='full'>
+// 				<SearchList />
+// 			</Box>
+// 		</>
+// 	);
+// };
 
 export default Search;
