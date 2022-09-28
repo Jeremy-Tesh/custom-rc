aa38e66eeee9e1e471c90bb0459c07696335b622
jeremy-tesh <jermitesh20@gmail.com>
2022-09-28 11:03:36 +0300

    disable ee feature

--- apps/meteor/client/views/hooks/useUpgradeTabParams.ts
+++ apps/meteor/client/views/hooks/useUpgradeTabParams.ts
@@ -1,4 +1,4 @@
-import { useSetting, useEndpoint } from '@rocket.chat/ui-contexts';
+import { useEndpoint } from '@rocket.chat/ui-contexts';
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 
@@ -7,22 +7,22 @@ import { UpgradeTabVariant, getUpgradeTabType } from '../../../lib/getUpgradeTab
 export const useUpgradeTabParams = (): { tabType: UpgradeTabVariant | false; trialEndDate: string | undefined; isLoading: boolean } => {
 	const getRegistrationStatus = useEndpoint('GET', '/v1/cloud.registrationStatus');
 	const getLicenses = useEndpoint('GET', '/v1/licenses.get');
-	const cloudWorkspaceHadTrial = useSetting('Cloud_Workspace_Had_Trial') as boolean;
+	// const cloudWorkspaceHadTrial = useSetting('Cloud_Workspace_Had_Trial') as boolean;
 
 	const { data: registrationStatusData } = useQuery(['registrationStatus'], () => getRegistrationStatus());
 	const { data: getValidLicensesData, isSuccess } = useQuery(['licenses'], () => getLicenses(), {
 		enabled: !!registrationStatusData,
 	});
 
-	const { registrationStatus } = registrationStatusData ?? {};
+	// const { registrationStatus } = registrationStatusData ?? {};
 	const { licenses } = getValidLicensesData ?? {};
 
-	const registered = registrationStatus?.workspaceRegistered ?? false;
-	const hasValidLicense = (licenses?.length ?? 0) > 0;
-	const hadExpiredTrials = cloudWorkspaceHadTrial ?? false;
+	const registered = true;
+	const hasValidLicense = true;
+	const hadExpiredTrials = false;
 
 	const trialLicense = licenses?.find(({ meta }) => meta?.trial);
-	const isTrial = licenses?.every(({ meta }) => meta?.trial) ?? false;
+	const isTrial = false;
 	const hasGoldLicense = licenses?.some(({ tag }) => tag?.name === 'Gold') ?? false;
 	const trialEndDate = trialLicense?.meta ? format(new Date(trialLicense.meta.trialEnd), 'yyyy-MM-dd') : undefined;
 
